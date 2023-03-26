1225012c2dc2f7b90b6b1e78bc689dd29f1ac6c0
felsef <felsef@protonmail.com>
2023-03-26 04:06:23 +0100

    2nd commit

---
+++ src/deck.py
@@ -0,0 +1,19 @@
+import sqlite3
+from table import Table
+class Deck(Table):
+    
+    def __init__(self, init=False):
+        #Constructor for Deck() class
+        pass
+
+    def __repr__(self):
+        #Represent Deck() class as a str
+        pass
+    
+    def selectDeck(self):
+        #Select deck from sql database
+        pass
+
+    def addCard(self, front, back):
+        #Add card to deck
+        pass
---
+++ src/table.py
@@ -0,0 +1,7 @@
+class Table:
+    def __init__(self):
+        pass
+
+    def list(self):
+        #Used to list all decks - is inherited by Deck() class
+        pass
