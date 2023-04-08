228816d8fb88fe67ee60e5b4bf4a720f75614d5e
felsef <felsef@protonmail.com>
2023-04-08 10:17:38 +0100

    final com

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 # flashpy
 A learning and memorisation tool that uses the active-recall method of memorisation which is an evidence based technique to achieve the highest scores on exams and other academic structures.
-
-## installation
-```bash
-pip install flashpy
-```
-
-## requirements
-* colorama
---
+++ test_interface.py
@@ -0,0 +1,37 @@
+from src.deck import Deck
+from os import system
+
+l = ["Add Card", "Remove Card", "Shuffle Deck", "List Cards", "Exit"]
+
+def main():
+    system("clear")
+    print("===Flashpy=CLI===")
+    deckname = str(input("Deck Name > "))
+    deck = Deck(deckname)
+    while True:
+        for i, x in enumerate(l):print(f"{i + 1} - {x}")
+        inp = input(" > ")
+        if inp == '1':
+            f = input("Enter Front > ")
+            b = input("Enter Back > ")
+            deck.addCard(f, b)
+            system("clear")
+            print("Card was added to deck")
+        elif inp == '2':
+            pk = int(input("Enter PK of Card > "))
+            deck.delCard(pk)
+            system("clear")
+            print("Card was removed from deck")
+            #delete card in deck
+        elif inp == '3':
+            deck.shuffle()
+            system("clear")
+            #play the game
+        elif inp == '4':
+            deck.pasteDeck()
+        elif inp == '5':
+            print("Exiting Flashpy...")
+            exit()
+
+if __name__ == "__main__":
+    main()
